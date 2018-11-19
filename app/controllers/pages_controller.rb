5ed1fa4ecb8ddc0fe5fd4664ecd16febd0e40b12
Omar Iván <omarivanfq@gmail.com>
2018-11-18 23:50:46 -0600

    Followed cats posts on feed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
   def home
-    @posts = Post.all
+    @posts = Post.where(cat_id: current_user.following.ids)
   end
 end
