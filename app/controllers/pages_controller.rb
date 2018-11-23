e73bd9a2c7abfbe3a6c2bdaa1341258461c01173
Omar Iván <omarivanfq@gmail.com>
2018-11-23 01:11:12 -0600

    Posts shared and created mixed and sorted

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,16 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
   def home
-      @posts = Post.where(cat_id: current_user.following.ids)
+    created_posts = Post.where(cat_id: current_user.following.ids)
+    shared = Share.where(profile_id: current_user.following.map {|c| c.profile.id}).to_a
+    show_posts = []
+    created_posts.each do |post|
+      while shared.first != nil && shared.first.created_at < post.created_at do
+        show_posts.push(shared.shift.post)
+      end
+      show_posts.push(post)
+    end
+    @posts = show_posts.reverse
   end
   def search
     @tag = Tag.find_by(name: params[:tag])
