b34afc353875c6fc9b179d3ee504ffcdcedc76c0
Omar Iván <omarivanfq@gmail.com>
2018-11-29 01:23:28 -0600

    Comments dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :cats, :dependent => :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
-  has_many :comments
+  has_many :comments, :dependent => :destroy
   has_many :favorites
   has_many :posts, through: :favorites
 
